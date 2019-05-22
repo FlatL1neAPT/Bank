b679f38a745b74af805223f6b7db154d1930e271
kogayra <ra.kogay@tensor.ru>
2019-05-22 11:40:22 +0300

    Сохранение состояний

--- TinkoffBank.py
+++ TinkoffBank.py
@@ -24,23 +24,25 @@ class TinkoffBank(Bank):
         return True
 
     def is_in_odp(self, inn):
-        from datetime import datetime
+        #from datetime import datetime
 
-        print(datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S") + " " + str(inn))
+        #print(datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S") + " " + str(inn))
 
-        request = {"fields": {"inn": inn},
-                   "inn_checked_status": 4,
-                   "errors": {"inn": False}}
+        #request = {"fields": {"inn": inn},
+        #           "inn_checked_status": 4,
+        #           "errors": {"inn": False}}
 
-        url = "https://ats.tinkoff.ru/uif/partner/f8f513aefb7f3feb47e219995f7d3711/check_inn/"
+        #url = "https://ats.tinkoff.ru/uif/partner/f8f513aefb7f3feb47e219995f7d3711/check_inn/"
 
-        res = requests.post(url, json=request)
-        response = json.loads(res.text)
+        #res = requests.post(url, json=request)
+        #response = json.loads(res.text)
 
-        if response['result'] == "Duble":
-            return True
-        else:
-            return False
+        #if response['result'] == "Duble":
+        #    return True
+        #else:
+        #    return False
+
+        return False
 
     def get_score_id(self, inn):
 
